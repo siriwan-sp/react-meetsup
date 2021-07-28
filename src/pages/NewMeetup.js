7952dba714581054fb04d23eb64db688445d5ae5
siriwan.sp <siriwan.sp36@gmail.com>
2021-07-28 22:10:08 +0700

    first commit

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,29 @@
+import { useHistory } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-gettig-starte-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/"); //user Add data เสร็จแล้วเว็บจะลิงค์ไปหน้าแรก /
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
